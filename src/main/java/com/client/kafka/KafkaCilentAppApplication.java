a34c1e6f6af6c6a4b5632c112f1341fcc04ed473
vishal-3107 <vishalkumarsingh028@gmail.com>
2025-03-04 03:48:37 +0530

    Added Consumer Service

---
+++ src/main/java/com/client/kafka/KafkaCilentAppApplication.java
@@ -0,0 +1,13 @@
+package com.client.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCilentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCilentAppApplication.class, args);
+	}
+
+}
